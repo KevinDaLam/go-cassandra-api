4a7db45082ef75847b2d6ec0389a78a947dafbbe
LAM <ar-kevin.lam@p25353.intra.rakuten.co.jp>
2018-09-12 10:46:00 +0900

    Basic go and cassandra routing api through http requests

---
+++ cassandra/main.go
@@ -0,0 +1,22 @@
+package cassandra
+
+import (
+	"fmt"
+
+	"github.com/gocql/gocql"
+)
+
+var Session *gocql.Session
+
+func init() {
+	var err error
+
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Keyspace = "streamdemo"
+	Session, err = cluster.CreateSession()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Cassandra Init Done")
+
+}
